a89ef508b8ff4fe78e2818c89d6e09b1cafc65b2
Ryan Nelson <xoplefs@gmail.com>
2020-08-07 15:12:30 -0700

    Renames S2 files

--- S2 if_example.py
+++ S2 if_example.py
@@ -1,2 +1,6 @@
-name = input()
-print(name)
+name = 'Alice'
+if name == 'Alice':
+    print('Hi, ' + name)
+    
+else:
+    print('Hey, fool! Get outta here!')
--- if_example.py
+++
@@ -1,6 +0,0 @@
-name = 'Alice'
-if name == 'Alice':
-    print('Hi, ' + name)
-    
-else:
-    print('Hey, fool! Get outta here!')
